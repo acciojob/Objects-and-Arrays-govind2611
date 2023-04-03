fd7e69360446da23a25f7062f9820848bab65105
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-04-03 19:41:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Your Script here.
+const players = ['John', 'Bob', 'Alice', 'Poppy'];
+
+const person = {
+      name: 'John Doe',
+      age: 80
+    };
+
+// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+
+
+export {players,person,team,team1,cap1}
