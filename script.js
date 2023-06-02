641140733f61eec813563b5a6699fe0a27427552
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-06-02 11:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const players = ['John', 'Bob', 'Alice', 'Poppy'];
+const players = ["John", "Bob", "Alice", "Poppy"];
 
 const person = {
-      name: 'John Doe',
-      age: 80
-    };
+  name: "John Doe",
+  age: 80,
+};
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-const team = players;
-const team1 = [...players];
-const cap1 = {...person};
-
-export {players,person,team,team1,cap1}
+	let team = players;
+    let team1 = ["John", "Bob", "Alice", "Poppy"];
+	let cap1 = {
+	  name: "John Doe",
+	  age: 80,
+	};
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
